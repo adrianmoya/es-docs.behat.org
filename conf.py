83cfcf1c143b4389c93c4fa197bc354289760bfd
everzet <ever.zet@gmail.com>
2012-06-01 10:35:59 +0200

    updated theme to be universal

--- conf.py
+++ conf.py
@@ -104,6 +104,11 @@ html_theme = 'scrolls'
 # documentation.
 #html_theme_options = {}
 
+html_context = {
+    'logo_name':     'behat',
+    'menu_section':  'behat-docs'
+}
+
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
 
